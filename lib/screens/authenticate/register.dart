a37f87e7b995e08ee62e02068a4870e7b66521da
Jedrick Espiritu <Jed.Espiritu.000@gmail.com>
2025-02-15 12:06:44 -0800

    #11 added a toggle between sign up and sign in pages

---
+++ lib/screens/authenticate/register.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_project/services/auth.dart';
+
+class Register extends StatefulWidget {
+
+  final Function switchWidget;
+  const Register({super.key, required this.switchWidget});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+
+  final AuthService _auth = AuthService();
+
+  // text field state
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        // Elevation removes the drop shadow, as the Scaffold widget is no longer elevated off the screen
+        elevation: 0.0,
+        title: Text('Sign up to Brew Crew'),
+        actions: <Widget>[
+            TextButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Sign In'),
+              onPressed: () {
+                widget.switchWidget();
+              },
+            ),
+          ]
+      ),
+      // The actual body of our widget
+      body: Container(
+        // This will make the widget not touch the edges of the screen
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height:20.0),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val);
+                }
+              ),
+              SizedBox(height:20.0),
+              TextFormField(
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                }
+              ),
+              SizedBox(height:20.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink[400],
+                  foregroundColor: Colors.pink[400],
+                ),
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () async {
+                  print(email);
+                  print(password);
+              },
+              )
+            ]
+          )
+        ),
+
+      ),
+    );
+  }
+}
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:my_first_project/services/auth.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({super.key});
+
+  final Function switchWidget;
+  const SignIn({super.key, required this.switchWidget});
 
   @override
   State<SignIn> createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
-  
   final AuthService _authenticator = AuthService();
 
   // text field state
@@ -21,50 +22,51 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        // Elevation removes the drop shadow, as the Scaffold widget is no longer elevated off the screen
-        elevation: 0.0,
-        title: Text('Sign in to Brew Crew')
-      ),
+          backgroundColor: Colors.brown[400],
+          // Elevation removes the drop shadow, as the Scaffold widget is no longer elevated off the screen
+          elevation: 0.0,
+          title: Text('Sign in to Brew Crew'),
+          actions: <Widget>[
+            TextButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('Register'),
+              onPressed: () {
+                widget.switchWidget();
+              },
+            ),
+          ]),
       // The actual body of our widget
       body: Container(
         // This will make the widget not touch the edges of the screen
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
         child: Form(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height:20.0),
-              TextFormField(
-                onChanged: (val) {
-                  setState(() => email = val);
-                }
-              ),
-              SizedBox(height:20.0),
-              TextFormField(
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                }
-              ),
-              SizedBox(height:20.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.pink[400],
-                  foregroundColor: Colors.pink[400],
-                ),
-                child: Text(
-                  'Sign in',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  print(email);
-                  print(password);
-              },
-              )
-            ]
+            child: Column(children: <Widget>[
+          SizedBox(height: 20.0),
+          TextFormField(onChanged: (val) {
+            setState(() => email = val);
+          }),
+          SizedBox(height: 20.0),
+          TextFormField(
+              obscureText: true,
+              onChanged: (val) {
+                setState(() => password = val);
+              }),
+          SizedBox(height: 20.0),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.pink[400],
+              foregroundColor: Colors.pink[400],
+            ),
+            child: Text(
+              'Sign in',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              print(email);
+              print(password);
+            },
           )
-        ),
-
+        ])),
       ),
     );
   }
